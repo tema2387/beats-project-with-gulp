6ba87beb34002f30b92bb5bcdb5d963238ca4fda
tema2387 <82901481+tema2387@users.noreply.github.com>
2021-06-21 23:23:36 +0300

    Add files via upload

--- beats-project/menu.js
+++ beats-project/menu.js
@@ -1,6 +1,7 @@
 const openButton = document.querySelector (".hamburger");
 const closeButton = document.querySelector (".close-button");
 const menu = document.querySelector (".menu");
+let item = document.querySelectorAll(".menu__item");
 
 
 openButton.addEventListener ("click", e => {
@@ -9,6 +10,15 @@ openButton.addEventListener ("click", e => {
     closeButton.style.display = ("flex");
     openButton.style.display = ("none");
 })
+
+for (let i = 0; i< item.length; i++) {
+item[i].addEventListener ("click", e => {
+    e.preventDefault();
+    menu.style.display = ("none");
+    closeButton.style.display = ("none");
+    openButton.style.display = ("flex");
+})
+}
 
 closeButton.addEventListener ("click", e => {
     e.preventDefault();

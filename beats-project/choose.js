d43a1f564d0d343cf1c5ee667f7f8242161106d6
tema2387 <82901481+tema2387@users.noreply.github.com>
2021-06-27 01:35:58 +0300

    Add files via upload

---
+++ beats-project/choose.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    const slider = $(".choose__list").bxSlider({
+        pager: false,
+        controls:false
+    });
+
+    $(".choose__control-left--prev").click(e => {
+        e.preventDefault();
+        slider.goToPrevSlide();
+    })
+
+    $(".choose__control-right--next").click(e => {
+        e.preventDefault();
+        slider.goToNextSlide();
+    })
+});
+

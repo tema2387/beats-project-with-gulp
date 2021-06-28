4e0fe0842b23b28cb2fd4eb60b061c7c034cc17b
tema2387 <82901481+tema2387@users.noreply.github.com>
2021-06-28 14:34:34 +0300

    Add files via upload

---
+++ beats-project/js/team.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+    $(".team__item").on("click", function(e) {
+        $(this).toggleClass("active").siblings().removeClass("active");
+    })
+
+
+
+
+
+
+
+
+    // const openItem = function(item) {
+    //     const container = item.closest(".team__item");
+    //     const contentBlock = container.find(".team__description");
+
+    //     container.addClass("active");
+    //     contentBlock.height("100%");
+    // }
+
+    // const closeEveryItem = function(container) {
+    //     const items = container.find(".team__description");
+    //     const itemContainer = container.find(".team__item");
+
+    //     itemContainer.removeClass("active");
+    //     items.height(0);
+    // }
+
+    // $(".team__name").on("click", function(e){
+    //     e.preventDefault();
+    //     const $this = $(e.currentTarget);
+    //     const container = $this.closest(".team__list");
+    //     const elemContainer = $this.closest(".team__item");
+
+    //     if(elemContainer.hasClass("active")) {
+    //         closeEveryItem(container);
+    //     } else {
+    //         closeEveryItem(container);
+    //         openItem($this);
+    //     }
+    // })
+})

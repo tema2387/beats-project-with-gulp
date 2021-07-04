a13c930d179fba14025e5747c9b8608b7380fab1
tema2387 <82901481+tema2387@users.noreply.github.com>
2021-07-04 23:03:02 +0300

    Add files via upload

--- beats-project/js/pagination.js
+++ beats-project/js/pagination.js
@@ -96,7 +96,7 @@ $("[data-scroll-to]").click(e => {
 if (isMobile) {
     // https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
     $("body").swipe( {
-        swipe: function (event, direction,) {
+        swipe: function (event, direction) {
         const scroller = viewportScroller();
         let scrollDirection = "";
 

d4d1db08d5eaa4d681f1c42d87d0e51fb595e859
tema2387 <82901481+tema2387@users.noreply.github.com>
2021-07-04 22:58:00 +0300

    Add files via upload

--- beats-project/js/pagination.js
+++ beats-project/js/pagination.js
@@ -1,6 +1,9 @@
 const sections = $("section");
 const display = $(".wrapper__content");
 
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
 let inScroll = false;
 
 sections.first().addClass("section--active")
@@ -78,6 +81,8 @@ $(window).on("keydown", e => {
     }
 });
 
+$(".wrapper").on("touchmove", e => e.preventDefault());
+
 $("[data-scroll-to]").click(e => {
     e.preventDefault();
 
@@ -86,5 +91,23 @@ $("[data-scroll-to]").click(e => {
     const reqSection = $(`[data-section-id=${target}]`);
 
     performTransition(reqSection.index());
-})
+});
+
+if (isMobile) {
+    // https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+    $("body").swipe( {
+        swipe: function (event, direction,) {
+        const scroller = viewportScroller();
+        let scrollDirection = "";
+
+        if(direction == "up") scrollDirection = "next";
+        if(direction == "down") scrollDirection = "prev";
+
+        scroller[scrollDirection] ();
+        },
+    });
+}
+
+
+
 

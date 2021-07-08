3ff6444dc6656c8c0a7bbb7f838b48d1de9ff993
tema2387 <82901481+tema2387@users.noreply.github.com>
2021-07-08 05:05:58 +0300

    Add files via upload

---
+++ beats-project/gulpfile.js
@@ -0,0 +1,98 @@
+const { src, dest, task, series, watch, parallel } = require("gulp");
+const rm = require("gulp-rm");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload
+const sassGlob = require("gulp-sass-glob");
+const autoprefixer = require("gulp-autoprefixer");
+const px2rem = require("gulp-smile-px2rem");
+const gcmq = require("gulp-group-css-media-queries");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const gulpif = require("gulp-if");
+
+const env = process.env.NODE_ENV;
+
+const {DIST_PATH, SRC_PATH, STYLES_LIBS, JS_LIBS} = require("./gulp.config");
+
+task( "clean", () => {
+    return src( `${DIST_PATH}/**/*`, { read: false }).pipe( rm());
+})
+
+const files = [
+    "src/css/**/*.scss",
+    "!src/css/main.scss",
+];
+
+task("copy", () => {
+    return src(files).pipe(dest(DIST_PATH));
+})
+
+task("copy:html", () => {
+    return src(`${SRC_PATH}/*.html`).pipe(dest(DIST_PATH)).pipe(reload({ stream: true }));
+})
+
+
+task("styles", () => {
+    return src([...STYLES_LIBS,"src/css/main.scss"])
+    .pipe(gulpif(env == "dev", sourcemaps.init()))
+    .pipe(concat("main.min.scss"))
+    .pipe(sassGlob())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(px2rem())
+    .pipe(gulpif (env == "dev" , autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: true
+    })))
+    .pipe(gulpif(env == "prod", gcmq()))
+    .pipe(gulpif(env == "prod", cleanCSS()))
+    .pipe(gulpif(env === "dev", sourcemaps.write()))
+    .pipe(dest(DIST_PATH))
+    .pipe(reload({stream: true}));
+});
+
+task ("scripts", () => {
+    return src([...JS_LIBS,"src/js/*.js"])
+    .pipe(gulpif(env === "dev",sourcemaps.init()))
+    .pipe(concat("main.min.js", {newLine: ";"}))
+    .pipe(gulpif(env === "prod", babel({
+        presets: ["@babel/env"]
+    })))
+    .pipe(gulpif(env === 'prod', uglify()))
+    .pipe(gulpif(env === 'dev', sourcemaps.write()))
+    .pipe(dest(DIST_PATH))
+    .pipe(reload({stream: true}));
+})
+
+task("img", () => {
+    return src("src/img/**")
+    .pipe(dest(`${DIST_PATH}/img`));
+})
+
+task("fonts", () => {
+    return src("src/fonts/*")
+    .pipe(dest(`${DIST_PATH}/fonts`));
+})
+
+
+task("server", () => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        open: false
+    });
+});
+
+task("watch", () => {
+    watch("./src/css/**/*.scss",  series("styles"));
+    watch("./src/*.html", series("copy:html"));
+    watch("./src/js/*.js", series("scripts"));
+})
+
+
+task("default", series("clean", parallel("copy:html", "styles", "scripts","img", "fonts"), parallel("watch", "server")));
+task("build", series("clean", parallel("copy:html", "styles", "scripts", "img", "fonts")))
